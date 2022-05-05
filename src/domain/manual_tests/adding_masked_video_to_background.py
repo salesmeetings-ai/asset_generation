2b2420370b354c5681451b904ffcc25548cd2d09
taraskuzyk <taras@leadstheory.com>
2022-05-05 11:07:33 -0600

    added flask server draft

--- src/domain/manual_tests/adding_masked_video_to_background.py
+++ src/domain/manual_tests/adding_masked_video_to_background.py
@@ -6,20 +6,31 @@ from domain.position import Position
 from domain.video_clip import VideoClip
 
 
-def get_execution_time(func, args, kwargs):
-    beginning = time.perf_counter()
-    func(*args, **kwargs)
-    end = time.perf_counter()
-    print(f"Execution time: {end-beginning}")
+def timetest(input_func):
+    def timed(*args, **kwargs):
+        start_time = time.time()
+        result = input_func(*args, **kwargs)
+        end_time = time.time()
+        print(
+            f"Method Name - {input_func.__name__}, "
+            f"Args - {args}, Kwargs - {kwargs}, "
+            f"Execution Time - {end_time - start_time}"
+        )
+
+        return result
+
+    return timed
 
 
+@timetest
 def create_video(threads=1):
     josiah_clip: VideoClip = VideoClip.from_path(
         path=Path("adding_masked_video_input_video.mp4")
     )
-    josiah_clip.add_circle_mask(radius=int(josiah_clip.get_dimensions().width / 2 - 7))
+    add_cricle_mask_to_video(josiah_clip)
 
-    josiah_clip.save(Path("adding_masked_video_circle_crop.mp4"), threads=threads)
+    save_circle_mask_video(josiah_clip, threads)
+
     background_clip = VideoClip.from_path(path=Path("genium_slide_LATAM.gif"))
     background_clip.change_speed(final_duration=josiah_clip.duration)
     position = Position(
@@ -27,18 +38,32 @@ def create_video(threads=1):
         - josiah_clip.get_dimensions().height,
         horizontal=0,
     )
-    background_clip.add_overlay(josiah_clip, position)
+
+    add_overlay_to_gif(background_clip, josiah_clip, position)
+
+    save_entire_clip(background_clip, threads)
+
+
+@timetest
+def save_entire_clip(background_clip, threads):
     background_clip.save(Path("adding_masked_video_result.mp4"), threads=threads)
 
-    with open(Path("adding_masked_video_result.mp4")) as image:
-        webbrowser.open_new(image.name)
+
+@timetest
+def add_overlay_to_gif(background_clip, josiah_clip, position):
+    background_clip.add_overlay(josiah_clip, position)
 
 
-def get_performance_test_results():
-    # get_execution_time(create_video, [1], {})
-    get_execution_time(create_video, [0], {})
+@timetest
+def save_circle_mask_video(josiah_clip, threads):
+    josiah_clip.save(Path("adding_masked_video_circle_crop.mp4"), threads=threads)
+
+
+@timetest
+def add_cricle_mask_to_video(josiah_clip):
+    josiah_clip.add_circle_mask(radius=int(josiah_clip.get_dimensions().width / 2 - 7))
 
 
 if __name__ == "__main__":
 
-    get_performance_test_results()
+    create_video(threads=0)
---
+++ src/entry_points/flask_server.py
@@ -0,0 +1,61 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/assemble_video")
+def generate_vid():
+    # concatenate
+    # add circle mask
+    #  mask
+    # concatenate
+    return "<p>Hello, World!</p>"
+
+
+NATO_STANDARTS = {
+    "output_format": "mp4",
+    "source_files": {
+        "video_1": {"hex": "abcdef", "format": "wav", "type": "mkv"},
+        "background_image": {"hex": "abcddd", "format": "wav", "type": "mkv"},
+        "lip_sync_audio": {"hex": "abcddd", "format": "wav", "type": "mkv"},
+    },
+    "video_components": {
+        0: [
+            {
+                "source_file": "background_image",
+                "slice": {"start": 0, "end": 2},
+                "position": [0, 0],
+            },
+        ],
+        1: [
+            {
+                "source_file": "video_1",
+                "position": [0, 0],
+                "commands": [
+                    {"slice": [0, 2]},
+                    {"resize": [150, 150]},
+                    {"circle_mask": ""},
+                ],
+            },
+            {  # lip sync part
+                "source_file": "video_1",
+                "position": [0, 0],
+                "commands": [
+                    {"slice": [2, 3]},
+                    {"lip_sync": "lip_sync_audio"},
+                    {"resize": [150, 150]},
+                    {"circle_mask": ""},
+                ],
+            },
+            {
+                "source_file": "video_1",
+                "position": [0, 0],
+                "commands": [
+                    {"slice": [3, "end"]},
+                    {"resize": [150, 150]},
+                    {"circle_mask": ""},
+                ],
+            },
+        ],
+    },
+}
